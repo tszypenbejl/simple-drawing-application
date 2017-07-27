9e3a2c93b878c9d5a41fb4c961122b708c70f809
Tomasz Szypenbejl <tomasz.szypenbejl@interia.pl>
2017-07-27 23:26:15 +0200

    Attempt to make the lines smooth. Swap colors button added.

--- src/main/scala/io/github/tszypenbejl/simpledrawingapplication/DrawingApp.scala
+++ src/main/scala/io/github/tszypenbejl/simpledrawingapplication/DrawingApp.scala
@@ -10,6 +10,7 @@ import scalafx.scene.control.{Button, ColorPicker}
 import scalafx.scene.input.MouseEvent
 import scalafx.scene.layout._
 import scalafx.scene.paint.Color
+import scalafx.scene.shape.StrokeLineCap
 
 
 private class DrawingCanvas(size: Region) extends Canvas {
@@ -17,6 +18,7 @@ private class DrawingCanvas(size: Region) extends Canvas {
   private val halfBrushSize = brushSize / 2
   private val g = graphicsContext2D
   private var prevPoint: (Double, Double) = null
+  private var prevPrevPoint: (Double, Double) = null
 
   private def clearRect(x: Double, y: Double, w: Double, h: Double) = {
     g.fill = backgroundColor.value
@@ -26,14 +28,29 @@ private class DrawingCanvas(size: Region) extends Canvas {
   private def drawBrush(x: Double, y: Double, useBackgroundColor: Boolean) = {
     g.fill = if (useBackgroundColor) backgroundColor.value else brushColor.value
     g.fillOval(x - halfBrushSize, y - halfBrushSize, brushSize, brushSize)
+    prevPrevPoint = null
     prevPoint = (x, y)
   }
 
   private def drawLine(x: Double, y: Double, useBackgroundColor: Boolean) = {
-    if (prevPoint ne null) {
-      g.stroke = if (useBackgroundColor) backgroundColor.value else brushColor.value
-      g.lineWidth = brushSize
+//    def distance(p1: (Double, Double), p2: (Double, Double)) =
+//      math.sqrt(math.pow(p1._1 - p2._1, 2) + math.pow(p1._2 - p2._2, 2))
+    g.stroke = if (useBackgroundColor) backgroundColor.value else brushColor.value
+    g.lineCap = StrokeLineCap.Round
+    g.lineWidth = brushSize
+    if ((prevPrevPoint ne null) && (math.abs(x - prevPoint._1) + math.abs(y - prevPoint._2) > brushSize)) {
+      val controlPoint = (
+        prevPoint._1 + 0.2 * (prevPoint._1 - prevPrevPoint._1),
+        prevPoint._2 + 0.2 * (prevPoint._2 - prevPrevPoint._2)
+      )
+      g.beginPath()
+      g.moveTo(prevPoint._1, prevPoint._2)
+      g.quadraticCurveTo(controlPoint._1, controlPoint._2, x, y)
+      g.strokePath()
+      prevPrevPoint = controlPoint
+    } else if (prevPoint ne null) {
       g.strokeLine(prevPoint._1, prevPoint._2, x, y)
+      prevPrevPoint = prevPoint
     }
     prevPoint = (x, y)
   }
@@ -70,10 +87,13 @@ object DrawingApp extends JFXApp {
     title = "ScalaFX Simple paint application"
     minWidth = 100
     minHeight = 100
+    width = 500
+    height = 400
     scene = new Scene {
       private var drawingCanvas: DrawingCanvas = null
       private var colorPicker1: ColorPicker = null
       private var colorPicker2: ColorPicker = null
+      private var swapColorsButton : Button = null
       private var clearButton: Button = null;
       root = new VBox {
         children = Seq(
@@ -85,6 +105,7 @@ object DrawingApp extends JFXApp {
             children = Seq(
               { colorPicker1 = new ColorPicker(Color.Black); colorPicker1 },
               { colorPicker2 = new ColorPicker(Color.White); colorPicker2 },
+              { swapColorsButton = new Button("<->"); swapColorsButton },
               { clearButton = new Button("Clear"); clearButton }
             )
             clearButton.maxWidth = Double.MaxValue
@@ -94,6 +115,11 @@ object DrawingApp extends JFXApp {
       }
       drawingCanvas.brushColor <== colorPicker1.value
       drawingCanvas.backgroundColor <== colorPicker2.value
+      swapColorsButton.onAction = handle {
+        val tmp = colorPicker1.value.value
+        colorPicker1.value = colorPicker2.value.value
+        colorPicker2.value = tmp
+      }
       clearButton.onAction = handle { drawingCanvas.clear() }
     }
   }
