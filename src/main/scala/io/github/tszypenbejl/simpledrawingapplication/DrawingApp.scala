2c04ddc3ed7736740a519d84528bb694dcecd501
Tomasz Szypenbejl <tomasz.szypenbejl@interia.pl>
2017-08-11 13:00:20 +0200

    Another attempt to make the lines smooth

--- src/main/scala/io/github/tszypenbejl/simpledrawingapplication/DrawingApp.scala
+++ src/main/scala/io/github/tszypenbejl/simpledrawingapplication/DrawingApp.scala
@@ -1,5 +1,6 @@
 package io.github.tszypenbejl.simpledrawingapplication
 
+import scala.collection.mutable
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
@@ -13,12 +14,47 @@ import scalafx.scene.paint.Color
 import scalafx.scene.shape.StrokeLineCap
 
 
+private object MyGeom {
+  def lineEquationParams(p1: (Double, Double), p2: (Double, Double)) = {
+    val dx = p2._1 - p1._1
+    val dy = p2._2 - p1._2
+    assert(dx != 0 || dy != 0)
+    if (dy == 0)
+      (0.0, 1.0, p1._2)
+    else
+      (1.0, -dx / dy, p1._1 - p1._2 * dx / dy)
+  }
+
+  def intersection(line1p1: (Double, Double), line1p2: (Double, Double),
+                   line2p1: (Double, Double), line2p2: (Double, Double)) = {
+    val (a1, b1, c1) = lineEquationParams(line1p1, line1p2)
+    val (a2, b2, c2) = lineEquationParams(line2p1, line2p2)
+    val w = a1 * b2 - b1 * a2
+    val wx = c1 * b2 - b1 * c2
+    val wy = a1 * c2 - c1 * a2
+    if (w != 0)
+      (wx / w, wy / w)
+    else
+      (Double.NaN, Double.NaN)
+  }
+
+  def nyDistance(p1: (Double, Double), p2: (Double, Double)) = math.abs(p2._1 - p1._1) + math.abs(p2._2 - p1._2)
+
+  def sameDirection(p1: (Double, Double), p2: (Double, Double), p3: (Double, Double)) = {
+    val dx1 = p2._1 - p1._1
+    val dx2 = p3._1 - p2._1
+    val dy1 = p2._2 - p1._2
+    val dy2 = p3._2 - p2._2
+    math.signum(dx1) == math.signum(dx2) && math.signum(dy1) == math.signum(dy2)
+  }
+}
+
+
 private class DrawingCanvas(size: Region) extends Canvas {
   private val brushSize = 6
   private val halfBrushSize = brushSize / 2
   private val g = graphicsContext2D
-  private var prevPoint: (Double, Double) = null
-  private var prevPrevPoint: (Double, Double) = null
+  private var prevPoints = new mutable.ArrayBuffer[(Double, Double)]
 
   private def clearRect(x: Double, y: Double, w: Double, h: Double) = {
     g.fill = backgroundColor.value
@@ -28,31 +64,37 @@ private class DrawingCanvas(size: Region) extends Canvas {
   private def drawBrush(x: Double, y: Double, useBackgroundColor: Boolean) = {
     g.fill = if (useBackgroundColor) backgroundColor.value else brushColor.value
     g.fillOval(x - halfBrushSize, y - halfBrushSize, brushSize, brushSize)
-    prevPrevPoint = null
-    prevPoint = (x, y)
+    prevPoints.clear()
+    prevPoints.append((x, y))
   }
 
   private def drawLine(x: Double, y: Double, useBackgroundColor: Boolean) = {
-//    def distance(p1: (Double, Double), p2: (Double, Double)) =
-//      math.sqrt(math.pow(p1._1 - p2._1, 2) + math.pow(p1._2 - p2._2, 2))
     g.stroke = if (useBackgroundColor) backgroundColor.value else brushColor.value
     g.lineCap = StrokeLineCap.Round
     g.lineWidth = brushSize
-    if ((prevPrevPoint ne null) && (math.abs(x - prevPoint._1) + math.abs(y - prevPoint._2) > brushSize)) {
-      val controlPoint = (
-        prevPoint._1 + 0.2 * (prevPoint._1 - prevPrevPoint._1),
-        prevPoint._2 + 0.2 * (prevPoint._2 - prevPrevPoint._2)
-      )
+
+    if (prevPoints.size == 2 && MyGeom.nyDistance(prevPoints.last, (x, y)) <= brushSize)
+      prevPoints.remove(0, 1)
+
+    if (prevPoints.size == 1) {
+      g.strokeLine(prevPoints.head._1, prevPoints.head._2, x, y)
+    } else if (prevPoints.size == 3) {
+      val controlPoint = MyGeom.intersection(prevPoints(0), prevPoints(1), prevPoints(2), (x, y))
       g.beginPath()
-      g.moveTo(prevPoint._1, prevPoint._2)
-      g.quadraticCurveTo(controlPoint._1, controlPoint._2, x, y)
+      (g.moveTo _).tupled(prevPoints(1))
+      if (controlPoint._1.isNaN || controlPoint._2.isNaN || controlPoint._1.isInfinite || controlPoint._2.isInfinite
+          || MyGeom.nyDistance(controlPoint, (x, y)) > (width.value + height.value)
+          || !(MyGeom.sameDirection(prevPoints(0), prevPoints(1), controlPoint)
+                && MyGeom.sameDirection(controlPoint, prevPoints(2), (x, y)))) {
+          (g.lineTo _).tupled(prevPoints(2))
+      } else {
+        g.quadraticCurveTo(controlPoint._1, controlPoint._2, prevPoints(2)._1, prevPoints(2)._2)
+      }
+      g.lineTo(x, y)
       g.strokePath()
-      prevPrevPoint = controlPoint
-    } else if (prevPoint ne null) {
-      g.strokeLine(prevPoint._1, prevPoint._2, x, y)
-      prevPrevPoint = prevPoint
+      prevPoints.remove(0, 2)
     }
-    prevPoint = (x, y)
+    prevPoints.append((x, y))
   }
 
   onMousePressed = (e: MouseEvent) => drawBrush(e.x, e.y, e.secondaryButtonDown)
